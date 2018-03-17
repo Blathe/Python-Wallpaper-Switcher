7aded9f2b168cbc0697db6de0d065c47f35beee7
Scott Peters <scottpeters2281@gmail.com>
2018-03-16 18:35:07 -0700

    Update BGSwitcher.py
    Cleaned up some comments

--- BGSwitcher.py
+++ BGSwitcher.py
@@ -1,22 +1,24 @@
 import os,random,time
 
-#How often do you want the wallpaper to change, random number between these two values
-
+#Defines how often you want the background to cycle
 minimumTime = 1    #in minutes
 maximumTime = 2    #in minutes
 
 #picks a random time between minimum and maximum value
-switchBackgroundTimer = random.randrange(60*minimumTime, 60*maximumTime) 
+cycleBackgroundTimer = random.randrange(60*minimumTime, 60*maximumTime) 
 
 #directory of your pictures
-dir = "/home/USERNAME/Pictures/randomBackgrounds/"
+dir = "/home/..."
 
+#repeat while the program is running - program set to startup when system boots in the Linux startup program list.
 while True:
+	#picks a random file in the directory chosen above
 	backgroundChosen = random.choice(os.listdir(dir))
-
-	if backgroundChosen.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp')): #only image extensions
+	
+	#checks to see if the file chosen ends with common image extensions
+	if backgroundChosen.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp')): #image extensions
 		#set the background, then sleep until it's time to switch the background again
 		os.system("gsettings set org.mate.background picture-filename " + dir + backgroundChosen)
-		time.sleep(switchBackgroundTimer)
+		time.sleep(cycleBackgroundTimer)
 	
 	
